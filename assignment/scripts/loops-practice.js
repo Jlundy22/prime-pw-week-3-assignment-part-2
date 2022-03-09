1c98027fcd143360adf4fee9e18e89367cb5fa85
Jack Lund <jacklund920@gmail.com>
2022-03-09 09:40:22 -0600

    All questions complete in loops-practice.js

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -57,10 +57,15 @@ while (star < stars.length ) {
 
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
-let n = 0 
+let n = 0 ;
 while (n < 6) {
   console.log(n);
   n++;
 }
 // 3.c. TODO: Write a while loop to console.log the numbers from 10 to 5
 console.log('count backwards from 10 to 5');
+let num = 10;
+while (num>= 5) {
+  console.log(num);
+  num--;
+}
